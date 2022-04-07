2e0661af0707a58c68e810de6156872611c6cb05
Josh Moody <joshmoody24@gmail.com>
2022-04-06 18:21:55 -0600

    Database hotfix

--- CollisionsDB/CollisionsDB/Controllers/HomeController.cs
+++ CollisionsDB/CollisionsDB/Controllers/HomeController.cs
@@ -35,14 +35,14 @@ namespace CollisionsDB.Controllers
         }
 
         [HttpGet]
-        public IActionResult Summary(string county, int pageNum = 1)
+        public IActionResult Summary(int pageNum = 1)
         {
             int pageSize = 100;
 
             var x = new CollisionsViewModel
             {
                 Collisions = repo.Collisions
-                    .Where(c => c.County.CountyName == county || county == null)
+                    //.Where(c => c.Category == category || category == null)
                     .Include(c => c.City)
                     .Include(c => c.County)
                     .OrderBy(c => c.CrashId)
@@ -52,9 +52,7 @@ namespace CollisionsDB.Controllers
                 PageInfo = new PageInfo
                 {
                     TotalNumCrashes =
-                        (county == null
-                            ? repo.Collisions.Count()
-                            : repo.Collisions.Where(x => x.County.CountyName == county).Count()),
+                        (repo.Collisions.Count()),
                     CrashesPerPage = pageSize,
                     CurrentPage = pageNum
                 }
@@ -73,6 +71,13 @@ namespace CollisionsDB.Controllers
         [HttpPost]
         public IActionResult Calculator(CrashDataInput data)
         {
+
+            ViewBag.Result = GetSeverityPrediction(data);
+            return View();
+        }
+
+        private SeverityPrediction GetSeverityPrediction(CrashDataInput data)
+        {
             var result = session.Run(new List<NamedOnnxValue>
             {
                 NamedOnnxValue.CreateFromTensor("float_input", data.AsTensor())
@@ -80,8 +85,7 @@ namespace CollisionsDB.Controllers
             Tensor<float> score = result.First().AsTensor<float>();
             var prediction = new SeverityPrediction { PredictedValue = score.First() };
             result.Dispose();
-            ViewBag.Result = prediction;
-            return View();
+            return prediction;
         }
 
         public IActionResult Details(int collisionid)
@@ -90,6 +94,16 @@ namespace CollisionsDB.Controllers
                 .Include(c => c.City)
                 .Include(c => c.County)
                 .FirstOrDefault(x => x.CrashId == collisionid);
+
+            // predict what the crash severity SHOULD have been
+            CrashDataInput crashMLInput = CrashDataInput.CollisionToMLInput(crash);
+            float predictedSeverity = GetSeverityPrediction(crashMLInput).PredictedValue;
+            float actualSeverity = crash.CrashSeverityId;
+            float severityDifferencePercentage = actualSeverity / predictedSeverity;
+            ViewBag.SeverityDifference = severityDifferencePercentage;
+
+            // if this crash happened during the daytime, the severity would have decreased by 17%
+
             return View("Details", crash);
         }
     }
